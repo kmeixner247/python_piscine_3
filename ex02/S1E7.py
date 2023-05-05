0ba0c7d73b3a71e413e6a651e810ca25d72fd480
Konstantin Meixner <konstantin.meixner@freenet.de>
2023-05-05 23:35:08 +0200

    ex02

---
+++ ex02/S1E7.py
@@ -0,0 +1,30 @@
+from S1E9 import Character
+class Baratheon(Character):
+    """Representing the Baratheon family."""
+    def __init__(self, first_name, is_alive=True):
+        super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+    def __str__(self):
+        return f"Vector: ('{self.family_name}', '{self.hairs}', '{self.eyes}')"
+    def __repr__(self):
+        return f"Vector: ('{self.family_name}', '{self.hairs}', '{self.eyes}')"
+    def die(self):
+        self.is_alive = False
+
+class Lannister(Character):
+    """Representing the Lannister family."""
+    def __init__(self, first_name, is_alive=True):
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+    def __str__(self):
+        return f"Vector: ('{self.family_name}', '{self.hairs}', '{self.eyes}')"
+    def __repr__(self):
+        return f"Vector: ('{self.family_name}', '{self.hairs}', '{self.eyes}')"
+    def die(self):
+        self.is_alive = False
+    def create_lannister(first_name: str, is_alive=True):
+        return Lannister(first_name, is_alive)
