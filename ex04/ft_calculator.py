e014ff4ef7629fc0db373b2d6e0399d1cf1f291d
Konstantin Meixner <konstantin.meixner@freenet.de>
2023-05-06 01:34:11 +0200

    pretty much only tabs

--- ex04/ft_calculator.py
+++ ex04/ft_calculator.py
@@ -1,18 +1,19 @@
 class calculator:
     """A calculator class that does not need to be initialized and only \
 contains three static methods, which can be used to compute vectors with each \
-other
+other.
 
-Attributes:
-    None
+    Attributes:
+        None
 
-Methods:
-    - dotproduct(V1: list[float], V2: list[float]) -> None:
-        Static method to calculate the dotproduct of two vectors.
-    - add_vec(V1: list[float], V2: list[float]) -> None:
-        Static method to add two vectors together.
-    - sous_vec(V1: list[float], V2: list[float]) -> None:
-        Weirdly french named static method to subtract one vector from another
+    Methods:
+        - dotproduct(V1: list[float], V2: list[float]) -> None:
+            Static method to calculate the dotproduct of two vectors.
+        - add_vec(V1: list[float], V2: list[float]) -> None:
+            Static method to add two vectors together.
+        - sous_vec(V1: list[float], V2: list[float]) -> None:
+            Weirdly french named static method to subtract one vector from \
+another
 """
     @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
@@ -25,12 +26,12 @@ of the two vectors.
 For example, the dotproduct of the vectors (2, 5) and (7, 3) would be \
 (2 * 7, 5 * 3) = (14, 15)
 
-Args:
-    - V1 (list): The first vector in form of a list
-    - V2 (list): The second vector in form of a list
+    Args:
+        - V1 (list): The first vector in form of a list
+        - V2 (list): The second vector in form of a list
 
-Returns:
-    None
+    Returns:
+        None
 """
         dot = sum([a*b for (a, b) in zip(V1, V2)])
         print(f"Dot product is: {dot}")
@@ -42,12 +43,13 @@ Prints the result.
 
 For example: (2, 5) + (7, 3) = (2+7, 5+3) = (14, 8)
 
-Args:
-    - V1 (list): The first vector in form of a list
-    - V2 (list): The second vector in form of a list
+    Args:
+        - V1 (list): The first vector in form of a list
+        - V2 (list): The second vector in form of a list
 
-Returns:
-    None"""
+    Returns:
+        None
+"""
         sum = [float(a+b) for (a, b) in zip(V1, V2)]
         print(f"Add Vector is : {sum}")
 
@@ -58,11 +60,12 @@ name for some reason. Prints the result.
 
 For example: (2, 5) - (7, 3) = (2-7, 5-3) = (-5, 2)
 
-Args:
-    - V1 (list): The first vector in form of a list
-    - V2 (list): The second vector in form of a list
+    Args:
+        - V1 (list): The first vector in form of a list
+        - V2 (list): The second vector in form of a list
 
-Returns:
-    None"""
+    Returns:
+        None
+"""
         sub = [float(a-b) for (a, b) in zip(V1, V2)]
         print(f"Sous Vector is: {sub}")
